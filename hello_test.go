edf74e94bd1db27a7878bf29b847269e51a039a4
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-17 22:23:05 +0700

    refactor test for readable

--- hello_test.go
+++ hello_test.go
@@ -5,25 +5,21 @@ import (
 )
 
 func TestHelloWorld(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		result := Hello("")
-		expected := "Hello, world"
-
-		if result != expected {
-			t.Errorf("Need %s but got %s", expected, result)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Need %s but got %s", want, got)
 		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, want, got)
 	})
 
 	t.Run("say 'Hello, World', when empty string", func(t *testing.T) {
 		got := Hello("Pat")
 		want := "Hello, Pat"
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertCorrectMessage(t, want, got)
 	})
-}
-
-func TestHelloWithParameter(t *testing.T) {
-
 }
