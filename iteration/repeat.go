eb5348860c36068b3620fb582b3c976fa0596b5d
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-18 22:09:53 +0700

    refactor defaultRepeat to constant

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,10 @@
 package iteration
 
+var defaultRepeat = 5
+
 // Repeat return duplication message
 func Repeat(s string) (r string) {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < defaultRepeat; i++ {
 		r = r + s
 	}
 	return r
