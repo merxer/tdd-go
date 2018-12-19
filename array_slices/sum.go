0a20b6f3eccecfea7700c1f3612bbc4e8365ebc6
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-19 22:58:48 +0700

    summary array limit 5

--- array_slices/sum.go
+++ array_slices/sum.go
@@ -1,5 +1,10 @@
 package array_slices
 
-func Sum([5]int) int {
-	return 15
+func Sum(numbers [5]int) (sum int) {
+	sum = 0
+	for _, v := range numbers {
+		sum = sum + v
+		println(v)
+	}
+	return
 }
--- array_slices/sum_test.go
+++ array_slices/sum_test.go
@@ -2,11 +2,22 @@ package array_slices
 
 import "testing"
 
-func TestSum(t *testing.T) {
+func TestSumWhenInput1_to_5(t *testing.T) {
 	numbers := [5]int{1, 2, 3, 4, 5}
 
 	sum := Sum(numbers)
 	expected := 15
+
+	if sum != expected {
+		t.Errorf("expected '%d', but got '%d'", expected, sum)
+	}
+}
+
+func TestSumWhenInputWhenInput1_to_2(t *testing.T) {
+	numbers := [5]int{0, 1, 2, 3, 4}
+
+	sum := Sum(numbers)
+	expected := 10
 
 	if sum != expected {
 		t.Errorf("expected '%d', but got '%d'", expected, sum)
