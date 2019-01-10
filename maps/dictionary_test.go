b3fc23c595bd85f90237035a079eb442cffacfb2
Thawatchai Singngam <merxer.io@gmail.com>
2019-01-10 21:31:23 +0700

    add existing word

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -47,6 +47,16 @@ func TestAdd(t *testing.T) {
 		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, definition)
 	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
